52fbbd67320f9d701345c119ef3d1812446ce9a4
Frayna <frayna@Frayna>
2019-01-06 16:34:37 +0100

    [WIP] script mongo

---
+++ mongo-scripts/LoadChirurgies.js
@@ -0,0 +1,14 @@
+db.chirurgie.insert(
+	{
+		name : "Vaginoplastie",
+		type : "inversion pénienne",
+		description : "retounage de chaussette"
+	}
+)
+db.chirurgie.insert(
+	{
+		name : "Vaginoplastie",
+		type : "thai",
+		description : "retounage de sac a billes"
+	}
+)
